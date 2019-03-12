75bd8a282bf6567e18e54e92b9992c9de6dca54b
happy-yuxuan <victor.wuy@icloud.com>
2019-03-12 14:53:15 +0800

    更改第二周题目答案

--- homework/week2/mycat.c
+++ homework/week2/mycat.c
@@ -1,6 +1,7 @@
 #include "apue.h"
 #include <assert.h>
 #include <math.h>
+#include <stdlib.h>
 
 // #define	BUFFSIZE	4096
 
@@ -8,10 +9,10 @@ int
 main(int argc, char *argv[])
 {
 	int		n;
-	int 	BUFFSIZE = argc;
+	int 	BUFFSIZE = atoi(argv[1]);	// 将字符串转成整数在stdlib中
 	// assert( fabs(BUFFSIZE - (int)BUFFSIZE) <= 1e-8 );
 	char	buf[BUFFSIZE];
-
+	
 	while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
 		if (write(STDOUT_FILENO, buf, n) != n)
 			err_sys("write error");
